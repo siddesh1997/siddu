aee80a78da4d6e9836548baf181ead42002e54c8
Cloud User <ec2-user@ip-172-31-22-151.us-west-2.compute.internal>
2023-01-09 14:14:36 +0000

    sample create

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+class Helloworld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello World and welcome to java"); 
+    } 
+}
+
+
